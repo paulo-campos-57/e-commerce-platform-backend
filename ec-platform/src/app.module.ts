25e61d593fdcda5a855e68c4038586405fc0b29d
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2026-05-18 07:11:01 -0300

    add: include sqlite

--- ec-platform/src/app.module.ts
+++ ec-platform/src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'ec-platform.db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
