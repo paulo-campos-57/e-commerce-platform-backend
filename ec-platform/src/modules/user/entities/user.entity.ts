62a87515910ac7b5af04849473e731cc2a02c2f6
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2026-05-18 08:30:57 -0300

    add: create user entity

---
+++ ec-platform/src/modules/user/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'simple-array', nullable: true })
+  preferences: string[];
+}
