f9fa457797cb1b7149fda94b4fbbe85a9ed97b5a
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2026-05-18 09:03:06 -0300

    add: create user interface

---
+++ ec-platform/src/modules/user/interfaces/user-repository.interface.ts
@@ -0,0 +1,11 @@
+import { CreateUserDto } from "../dto/create-user.dto";
+import { UpdateUserDto } from "../dto/update-user.dto";
+import { User } from "../entities/user.entity";
+
+export interface IUserRepository {
+    createUser(data: CreateUserDto): Promise<User>;
+    findAll(): Promise<User[]>;
+    findByEmail(email: string): Promise<User | null>;
+    updateUser(id: string, data: UpdateUserDto): Promise<User | null>;
+    deleteUser(id: string): Promise<boolean>;
+}
